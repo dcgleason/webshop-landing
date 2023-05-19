1292c063d60ac454b7270af437d248688080cb6e
Dan Gleason <dannycgleason@gmail.com>
2023-05-19 14:02:08 -0400

    update to site map config

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,6 +1,9 @@
 module.exports = {
     siteUrl: 'https://www.webshopwizards.com',
     generateRobotsTxt: true, 
-    // ...other options
+    changefreq: 'daily',
+    priority: 0.7,
+    sitemapSize: 5000,
+    generateRobotsTxt: true,
   }
   
