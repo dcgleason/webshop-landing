e111a41df2a978e3f47d61730ab4c6d5b3616ef9
Dan Gleason <dannycgleason@gmail.com>
2023-05-20 08:50:39 -0400

    update

--- src/components/SecondaryFeatures.jsx
+++ src/components/SecondaryFeatures.jsx
@@ -46,7 +46,7 @@ const features = [
   {
     name: 'Expedited Delivery',
     summary:
-      'We deliver your feature requests in just a few business days on average.',
+      'We deliver requests in just a few business days on average.',
     description:
       "We know your time is valuable. That's why our team of Shopify Experts, 'wizards,' put our clients first and deliver quality results fast.",
     image: screenshotInventory,
