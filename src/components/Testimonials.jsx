a9d7c9c0034faacd387e4f1e5bac72efc15fe70b
Dan Gleason <dannycgleason@gmail.com>
2023-05-20 08:19:03 -0400

    update

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -79,12 +79,10 @@ export function Testimonials() {
       <Container>
         <div className="mx-auto max-w-2xl md:text-center">
           <h2 className="font-display text-3xl tracking-tight text-slate-900 sm:text-4xl">
-            Loved by businesses worldwide.
+            What others are saying
           </h2>
           <p className="mt-4 text-lg tracking-tight text-slate-700">
-            Our software is so simple that people can’t help but fall in love
-            with it. Simplicity is easy when you just skip tons of
-            mission-critical features.
+           Here are some of our past client and colleagues have said about us.
           </p>
         </div>
         <ul
