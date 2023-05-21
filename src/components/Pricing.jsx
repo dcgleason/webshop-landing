33db5a6d7b5ef8950fb73789117e1048d3eb3d50
Dan Gleason <dannycgleason@gmail.com>
2023-05-21 14:26:35 -0400

    update to pricing

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -118,7 +118,7 @@ export function Pricing() {
             name="Monthly"
             price="$1995/month"
             description="Paid monthly. Cancel anytime."
-            href="mailto:dan@webshopwizards.com?subject=New%20Client&body=I'd%20like%20to%20become%20a%20client%20billed%20monthly!"
+            href="mailto:dan@webshopwizards.com?subject=New%20Client&body=I'm%20ready%20to%20jump%20on%20a%2015%20minute%20call%20today%20or%20tomorrow%20to%20discuss%20my%20needs.%0AMy%20phone%20number%20is:%20(your%20phone%20number).%0ALook%20forward%20to%20hearing%20back.%20Thanks!"
             features={[
               'Unlimited revisions',
               'Logo and visual branding',
@@ -137,7 +137,7 @@ export function Pricing() {
             name="Quarterly"
             price="$1595/month"
             description="Paid quarterly for a 20% discount."
-            href="mailto:dan@webshopwizards.com?subject=New%20Client&body=I'd%20like%20to%20become%20a%20client%20billed%20quarterly!"
+            href="mailto:dan@webshopwizards.com?subject=New%20Client&body=I'm%20ready%20to%20jump%20on%20a%2015%20minute%20call%20today%20or%20tomorrow%20to%20discuss%20my%20needs.%0AMy%20phone%20number%20is:%20(your%20phone%20number).%0ALook%20forward%20to%20hearing%20back.%20Thanks!"
             features={[
               'Unlimited revisions',
               'Logo and visual branding',
@@ -155,7 +155,8 @@ export function Pricing() {
             name="Yearly"
             price="$1395/month"
             description="Paid yearly for a 30% discount."
-            href="mailto:dan@webshopwizards.com?subject=New%20Client&body=I'd%20like%20to%20become%20a%20client%20billed%20yearly!"
+            href="mailto:dan@webshopwizards.com?subject=New%20Client&body=I'm%20ready%20to%20jump%20on%20a%2015%20minute%20call%20today%20or%20tomorrow%20to%20discuss%20my%20needs.%0AMy%20phone%20number%20is:%20(your%20phone%20number).%0ALook%20forward%20to%20hearing%20back.%20Thanks!"
+
             features={[
               'Unlimited revisions',
               'Logo and visual branding',
