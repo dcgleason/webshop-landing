ed1f779f1b8d7eed23d01d1449a9a2c39caaebc5
Dan Gleason <dannycgleason@gmail.com>
2023-05-23 12:05:21 -0400

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,7 +28,7 @@ export function Hero() {
        
       </h1>
       <p className="mx-auto mt-6 max-w-2xl text-lg tracking-tight text-slate-700">
-      Shopify design and web development subscription services.  
+      Shopify design and web development subscription services for your app or storefront.  
       </p>
       <div className="mt-10 flex justify-center gap-x-6">
         <Button href="#pricing">See plans</Button>
