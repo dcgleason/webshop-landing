a136573ec05246ad4c52aa1b2bfb4750183d0497
Dan Gleason <dannycgleason@gmail.com>
2023-05-23 13:56:28 -0400

    update

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -14,12 +14,12 @@ export default function Home() {
   return (
     <>
         <Head>
-          <title>Webshop Wizards: Premium Shopify Web Development and Design Services</title>
+          <title>Webshop Wizards: Premium Web Development and Design Services</title>
           <meta
             name="description"
-            content="Webshop Wizards is a flexible, subscription-based Shopify web development and design agency. We prioritize user-friendly solutions without compromising accuracy, and provide ongoing service billed monthly. Experience the magic of our Shopify services today."
+            content="Webshop Wizards is a flexible, subscription-based web development and design agency. We prioritize user-friendly solutions without compromising accuracy, and provide ongoing service billed monthly. Experience the magic of our Shopify services today."
           />
-          <meta name="keywords" content="Shopify, Web Development, Design, Subscription-based, Webshop Wizards, E-commerce, Online Store, SEO, User-friendly, Ongoing Service"/>
+          <meta name="keywords" content="Web Development, Design, Subscription-based, Webshop Wizards, E-commerce, Online Store, SEO, User-friendly, Ongoing Service"/>
       </Head>
       <Header />
 
