25fe8950d194b6c60edb2cf42ede0ac777cab9b6
Dan Gleason <dannycgleason@gmail.com>
2023-05-23 15:19:18 -0400

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,7 +28,7 @@ export function Hero() {
        
       </h1>
       <p className="mx-auto mt-6 max-w-2xl text-lg tracking-tight text-slate-700">
-      One week sprints. One monthly fee. Unlimited requests. No contracts.  
+      One week sprints. One monthly fee. Unlimited requests. No meetings. No contracts.  
       </p>
       <div className="mt-10 flex justify-center gap-x-6">
         <Button href="#pricing">See plans</Button>
