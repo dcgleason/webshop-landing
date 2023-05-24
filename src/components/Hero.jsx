8c7932a45322628378e26f793565171ba1b88345
Dan Gleason <dannycgleason@gmail.com>
2023-05-24 07:04:51 -0400

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,7 +28,7 @@ export function Hero() {
        
       </h1>
       <p className="mx-auto mt-6 max-w-2xl text-lg tracking-tight text-slate-700">
-       Unlimited requests. No meetings. No contracts. One monthly fee. 
+       Fullstack support. Unlimited requests. No meetings. No contracts. One monthly fee. 
       </p>
       <div className="mt-10 flex justify-center gap-x-6">
         <Button href="#pricing">See plans</Button>
