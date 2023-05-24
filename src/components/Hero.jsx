5454e5eb372a861872282d89082b5393387774e1
Dan Gleason <dannycgleason@gmail.com>
2023-05-24 07:04:01 -0400

    udate

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,7 +13,7 @@ export function Hero() {
   return (
     <Container className="pb-16 pt-20 text-center lg:pt-32">
       <h1 className="mx-auto max-w-4xl font-display text-5xl font-medium tracking-tight text-slate-900 sm:text-7xl">
-      Fullstack Web-Development and Design {' '}
+      Web Development & Web Design {' '}
         <span className="relative whitespace-nowrap text-[#5271ff]">
           <svg
             aria-hidden="true"
