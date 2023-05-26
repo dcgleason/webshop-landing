f0c8b1ce26bb26111531545625bb1472da9d81dc
Dan Gleason <dannycgleason@gmail.com>
2023-05-26 17:27:19 -0400

    update to accordian

--- src/components/Faqs.jsx
+++ src/components/Faqs.jsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 
 import { Container } from '@/components/Container'
 import backgroundImage from '@/images/background-faqs.jpg'
+import { useState } from 'react';
 
 const faqs = [
   [
@@ -76,52 +77,69 @@ const faqs = [
 ]
 
 export function Faqs() {
+  const [activeIndex, setActiveIndex] = useState();
+
+  const handleClick = index => {
+    if (activeIndex === index) {
+      setActiveIndex(null);
+    } else {
+      setActiveIndex(index);
+    }
+  }
   return (
     <section
-      id="faq"
-      aria-labelledby="faq-title"
-      className="relative overflow-hidden bg-slate-50 py-20 sm:py-32"
-    >
-      <Image
-        className="absolute left-1/2 top-0 max-w-none -translate-y-1/4 translate-x-[-30%]"
-        src={backgroundImage}
-        alt=""
-        width={1558}
-        height={946}
-        unoptimized
-      />
-      <Container className="relative">
-        <div className="mx-auto max-w-2xl lg:mx-0">
-          <h2
-            id="faq-title"
-            className="font-display text-3xl tracking-tight text-slate-900 sm:text-4xl"
-          >
-            Frequently asked questions
-          </h2>
-          <p className="mt-4 text-lg tracking-tight text-slate-700">
-           If you have questions not listed here, please email us at <a className="underline"href="mailto:dan@webshopwizards.com">dan@webshopwizards.com</a> , and we will get back to you as soon as possible.
-          </p>
-        </div>
-        <ul
-          role="list"
-          className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-8 lg:max-w-none lg:grid-cols-3"
+    id="faq"
+    aria-labelledby="faq-title"
+    className="relative overflow-hidden bg-slate-50 py-20 sm:py-32"
+  >
+    <Image
+      className="absolute left-1/2 top-0 max-w-none -translate-y-1/4 translate-x-[-30%]"
+      src={backgroundImage}
+      alt=""
+      width={1558}
+      height={946}
+      unoptimized
+    />
+    <Container className="relative">
+      <div className="mx-auto max-w-2xl lg:mx-0">
+        <h2
+          id="faq-title"
+          className="font-display text-3xl tracking-tight text-slate-900 sm:text-4xl"
         >
-          {faqs.map((column, columnIndex) => (
-            <li key={columnIndex}>
-              <ul role="list" className="flex flex-col gap-y-8">
-                {column.map((faq, faqIndex) => (
+          Frequently asked questions
+        </h2>
+        <p className="mt-4 text-lg tracking-tight text-slate-700">
+          If you have questions not listed here, please email us at <a className="underline" href="mailto:dan@webshopwizards.com">dan@webshopwizards.com</a> , and we will get back to you as soon as possible.
+        </p>
+      </div>
+      <ul
+        role="list"
+        className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-8 lg:max-w-none lg:grid-cols-3"
+      >
+        {faqs.map((column, columnIndex) => (
+          <li key={columnIndex}>
+            <ul role="list" className="flex flex-col gap-y-8">
+              {column.map((faq, faqIndex) => {
+                const index = `${columnIndex}-${faqIndex}`;
+                return (
                   <li key={faqIndex}>
-                    <h3 className="font-display text-lg leading-7 text-slate-900">
+                    <h3
+                      className="font-display text-lg leading-7 text-slate-900 cursor-pointer"
+                      onClick={() => handleClick(index)}
+                    >
                       {faq.question}
                     </h3>
-                    <p className="mt-4 text-sm text-slate-700">{faq.answer}</p>
+                    <p className={`mt-4 text-sm text-slate-700 ${activeIndex === index ? '' : 'hidden'}`}>
+                      {faq.answer}
+                    </p>
                   </li>
-                ))}
-              </ul>
-            </li>
-          ))}
-        </ul>
-      </Container>
-    </section>
+                );
+              })}
+            </ul>
+          </li>
+        ))}
+      </ul>
+    </Container>
+  </section>
   )
 }
