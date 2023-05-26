7665d742fc085b10446d8d13968931fc466b75f4
Dan Gleason <dannycgleason@gmail.com>
2023-05-26 17:14:07 -0400

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -31,7 +31,7 @@ export function Hero() {
      Experienced devs. Unlimited requests. No meetings. One monthly fee. 
       </p>
       <div className="mt-10 flex justify-center gap-x-6">
-        <Button href="#pricing">Sign-up to get week 1 for free</Button>
+        <Button href="#pricing">Sign-up and get the first week free</Button>
         {/* <Button
           href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
           variant="outline"
