59bf1c76d624d7e8c8c2737f3a35292fb31d56d6
Dan Gleason <dannycgleason@gmail.com>
2023-06-18 14:57:38 -0400

    update to price

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -118,7 +118,7 @@ export function Pricing() {
           <Plan
             featured
             name="Monthly"
-            price="$3195/month"
+            price="$3995/month"
             description="Paid monthly if you love our work. Cancel anytime."
             href="https://calendly.com/webshopwizards/30min"
             features={[
