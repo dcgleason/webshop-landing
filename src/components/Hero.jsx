1d7236c266a80144b87d482ff4148b25e7be86cb
Dan Gleason <dannycgleason@gmail.com>
2023-06-29 19:36:53 -0400

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,7 +13,7 @@ export function Hero() {
   return (
     <Container className="pb-16 pt-20 text-center lg:pt-32">
       <h1 className="mx-auto max-w-4xl font-display text-5xl font-medium tracking-tight text-slate-900 sm:text-7xl">
-     ServiceNow development {' '}
+     ServiceNow developers {' '}
         <span className="relative whitespace-nowrap text-[#5271ff]">
           <svg
             aria-hidden="true"
@@ -28,7 +28,7 @@ export function Hero() {
        
       </h1>
       <p className="mx-auto mt-6 max-w-2xl text-lg tracking-tight text-slate-700">
-      Experienced devs. No contracts. Cancel or pause your subscription at anytime. 
+      Trusted senior resources. No contracts. Cancel or pause your subscription at anytime. 
       </p>
       <div className="mt-10 flex justify-center gap-x-6">
         <Button href="#pricing">View plans</Button>
--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -120,8 +120,8 @@ export function Pricing() {
           <Plan
             featured
             name="Monthly"
-            price="$3995"
-            description="Paid monthly if you love our work. Cancel anytime."
+            price="$6995"
+            description="Paid monthly. Cancel anytime."
             href="https://calendly.com/webshopwizards/30min"
             features={[
               'Unlimited revisions',
