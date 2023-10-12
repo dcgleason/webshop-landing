a4f29adf4be57d7ede899f9a0214ec9dd75aac12
dcgleason <danny.c.gleason@gmail.com>
2023-10-12 15:01:20 -0400

    update

--- src/components/SecondaryFeatures.jsx
+++ src/components/SecondaryFeatures.jsx
@@ -13,7 +13,7 @@ const features = [
     name: 'Easy onboarding',
     summary: 'Onboarding is a 30 minute phone call.',
     description:
-    "We'll send you a link to your ServiceNow IT Consultant's calendar, where you can book your first meeting and subsequent hours as needed.",
+    "Onboarding happens as your eariest availability. Will ask you a few questions about your ServiceNow work and determine fit.",
     image: screenshotProfitLoss,
     icon: function ReportingIcon() {
       let id = useId()
