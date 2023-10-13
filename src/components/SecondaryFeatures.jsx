75f51a5b5fc73b05cf16a20e4c8c0a5b3e319815
dcgleason <danny.c.gleason@gmail.com>
2023-10-13 08:09:19 -0400

    update

--- src/components/SecondaryFeatures.jsx
+++ src/components/SecondaryFeatures.jsx
@@ -216,7 +216,7 @@ export function SecondaryFeatures() {
       <Container>
         <div className="mx-auto max-w-2xl md:text-center">
           <h2 className="font-display text-3xl tracking-tight text-slate-900 sm:text-4xl">
-         Expert ServiceNow support when you need it.
+         Quality ServiceNow support when you need it.
           </h2>
           <p className="mt-4 text-lg tracking-tight text-slate-700">
         Our services include:
