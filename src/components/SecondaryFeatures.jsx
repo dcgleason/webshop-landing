67eb2e372574c9368205def2f2034d440859cf2a
dcgleason <danny.c.gleason@gmail.com>
2023-10-13 12:34:31 -0400

    udate

--- src/components/SecondaryFeatures.jsx
+++ src/components/SecondaryFeatures.jsx
@@ -46,7 +46,7 @@ const features = [
   {
     name: 'After-hours availability',
     summary:
-      'Overnight and evening support to meet your ServiceNow needs.',
+      'Evening and overnight support to meet your ServiceNow needs.',
     description:
       "Direct access to a Sr. ServiceNow support engineer, even after hours, so that you see resolution to your issues as soon as possible. 60-minute response times.",
     image: screenshotInventory,
