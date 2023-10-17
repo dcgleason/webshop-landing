67098170dca65c6493515dd5221c3294f86b4d77
dcgleason <danny.c.gleason@gmail.com>
2023-10-17 18:30:01 -0400

    update

--- src/components/SecondaryFeatures.jsx
+++ src/components/SecondaryFeatures.jsx
@@ -47,7 +47,7 @@ const features = [
     summary:
       'Insider access to ServiceNow partners & customers along with interview-prep.',
     description:
-      "We, as ServiceNow professionals oursevles, find & introduce you to key decision makers at ServiceNow partner and customers and then coach you on how to get referred and ace your interviews.",
+      "We, as ServiceNow professionals oursevles, find & introduce you to key decision makers at ServiceNow partners and customers and then coach you on how to get the job.",
     image: screenshotInventory,
     icon: function InventoryIcon() {
       return (
