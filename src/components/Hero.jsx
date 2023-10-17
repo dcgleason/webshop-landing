21325b7c2579d798199b9fd37d37aea94a209748
dcgleason <danny.c.gleason@gmail.com>
2023-10-17 19:22:14 -0400

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -31,7 +31,7 @@ export function Hero() {
        
       </h1>
       <p className="mx-auto mt-6 max-w-2xl text-lg tracking-tight text-slate-700">
-     Stop looking and start finding! BeyondNow LLC is a U.S.-based reverse recruitment firm that gets you your perfect job in the ServiceNow space.  
+     Stop looking and start finding! BeyondNow LLC is a U.S.-based reverse recruitment firm that will get you your perfect job in the ServiceNow space.  
       </p>
         <div className="mt-10 flex justify-center gap-x-6">
         <Button href="#pricing">View plans</Button>
