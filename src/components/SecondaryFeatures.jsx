b5f9382fab95bb6eb231ef6d36e4015bf8821f52
dcgleason <danny.c.gleason@gmail.com>
2023-10-17 18:32:42 -0400

    update

--- src/components/SecondaryFeatures.jsx
+++ src/components/SecondaryFeatures.jsx
@@ -215,7 +215,7 @@ export function SecondaryFeatures() {
       <Container>
         <div className="mx-auto max-w-2xl md:text-center">
           <h2 className="font-display text-3xl tracking-tight text-slate-900 sm:text-4xl">
-        Services include:
+       Our services include:
           </h2>
           <p className="mt-4 text-lg tracking-tight text-slate-700">
           </p>
