74ff0aa825d0311a155e5a487c917e526061a783
dcgleason <danny.c.gleason@gmail.com>
2023-10-17 20:53:30 -0400

    update

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -120,7 +120,7 @@ export function Pricing() {
             description="Pause or cancel anytime."
             href="https://buy.stripe.com/6oEcOUgHL4aqbPG3cc"
             features={[
-              '100% U.S.-based Reverse Recruiter (can service any English-speaking client)',
+              '100% U.S.-based Reverse Recruiter (require that clients plan to work in an English-speaking country)',
               'Initial discovery call with your Reverse Recruiter',
               '3-5 approved jobs applied to each week on your behalf',
               'Resume tailored for each submitted job application',
