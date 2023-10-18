c2b817f80ed836832caeead11a78036e700679d6
dcgleason <danny.c.gleason@gmail.com>
2023-10-18 12:51:03 -0400

    update

--- src/components/SecondaryFeatures.jsx
+++ src/components/SecondaryFeatures.jsx
@@ -56,6 +56,33 @@ const features = [
     description:
       "Your Reverse Recruiter (a ServiceNow professional) will find & introduce you to key decision makers at ServiceNow Partners companies and coach you on how to land interviews via referrals.",
     image: screenshotInventory,
+    icon: function ContactsIcon() {
+      return (
+        <>
+          <path
+            opacity=".5"
+            d="M25.778 25.778c.39.39 1.027.393 1.384-.028A11.952 11.952 0 0 0 30 18c0-6.627-5.373-12-12-12S6 11.373 6 18c0 2.954 1.067 5.659 2.838 7.75.357.421.993.419 1.384.028.39-.39.386-1.02.036-1.448A9.959 9.959 0 0 1 8 18c0-5.523 4.477-10 10-10s10 4.477 10 10a9.959 9.959 0 0 1-2.258 6.33c-.35.427-.354 1.058.036 1.448Z"
+            fill="#fff"
+          />
+          <path
+            d="M12 28.395V28a6 6 0 0 1 12 0v.395A11.945 11.945 0 0 1 18 30c-2.186 0-4.235-.584-6-1.605ZM21 16.5c0-1.933-.5-3.5-3-3.5s-3 1.567-3 3.5 1.343 3.5 3 3.5 3-1.567 3-3.5Z"
+            fill="#fff"
+          />
+        </>
+      )
+    },
+  },
+
+  {
+    name: 'Application Submitted for You & Interview Prep',
+    summary: (
+      <>
+        3-5 tailored job apps submitted per week <span className="underline">to get you the job</span>.
+      </>
+    ),
+    description:
+    "We handpick and apply to 3-5 jobs each week on your behalf (all submissions are tailored to each individual role) and then help you ace your interviews.",
+    image: screenshotContacts,
     icon: function InventoryIcon() {
       return (
         <>
@@ -71,32 +98,6 @@ const features = [
           />
           <path
             d="M8 10a1 1 0 0 1 1-1h18a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H9a1 1 0 0 1-1-1v-2Z"
-            fill="#fff"
-          />
-        </>
-      )
-    },
-  },
-  {
-    name: 'Application Submitted for You & Interview Prep',
-    summary: (
-      <>
-        3-5 tailored job apps submitted per week <span className="underline">to get you the job</span>.
-      </>
-    ),
-    description:
-    "We handpick and apply to 3-5 jobs each week on your behalf (all submissions are tailored to each individual role) and then help you ace your interviews.",
-    image: screenshotContacts,
-    icon: function ContactsIcon() {
-      return (
-        <>
-          <path
-            opacity=".5"
-            d="M25.778 25.778c.39.39 1.027.393 1.384-.028A11.952 11.952 0 0 0 30 18c0-6.627-5.373-12-12-12S6 11.373 6 18c0 2.954 1.067 5.659 2.838 7.75.357.421.993.419 1.384.028.39-.39.386-1.02.036-1.448A9.959 9.959 0 0 1 8 18c0-5.523 4.477-10 10-10s10 4.477 10 10a9.959 9.959 0 0 1-2.258 6.33c-.35.427-.354 1.058.036 1.448Z"
-            fill="#fff"
-          />
-          <path
-            d="M12 28.395V28a6 6 0 0 1 12 0v.395A11.945 11.945 0 0 1 18 30c-2.186 0-4.235-.584-6-1.605ZM21 16.5c0-1.933-.5-3.5-3-3.5s-3 1.567-3 3.5 1.343 3.5 3 3.5 3-1.567 3-3.5Z"
             fill="#fff"
           />
         </>
