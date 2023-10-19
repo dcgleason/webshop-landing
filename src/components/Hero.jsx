5e8585d62c50e34924fe5667eb10634e292898bf
dcgleason <danny.c.gleason@gmail.com>
2023-10-19 14:57:32 -0400

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ export function Hero() {
   return (
     <Container className="pb-16 pt-20 text-center lg:pt-32">
       <h1 className="mx-auto max-w-4xl font-display text-5xl font-medium tracking-tight text-slate-900 sm:text-7xl">
-    Looking for your next ServiceNow{' '}
+    Having trouble findingn your ServiceNow{' '}
         <span className="relative whitespace-nowrap text-[#235373]">
           <svg
             aria-hidden="true"
@@ -31,7 +31,7 @@ export function Hero() {
        
       </h1>
       <p className="mx-auto mt-6 max-w-2xl text-lg tracking-tight text-slate-700">
-     Stop looking and start finding! BeyondNow LLC is a U.S.-based reverse recruitment firm that is committed to landing you your dream job in the ServiceNow space.  
+     Stop looking and start finding! BeyondNow LLC is a U.S.-based reverse recruitment firm that is committed helping you stand out and landing you your dream job in the ServiceNow space.  
       </p>
         <div className="mt-10 flex justify-center gap-x-6">
         <Button href="#pricing">View plans</Button>
