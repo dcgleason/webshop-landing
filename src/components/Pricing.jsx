44931dac233346978333f9b83dae29c1a2052261
dcgleason <danny.c.gleason@gmail.com>
2023-10-25 14:51:17 -0400

    update

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -132,7 +132,7 @@ export function Pricing() {
             name="1-on-1 ServiceNow Career Coaching"
             price="$250"
             description="One-time call to answer your most pressing questions about your ServiceNow career search. Can talk about the following: "
-            href="https://buy.stripe.com/bIYg168bf6iy3jaeV1"
+            href="https://calendly.com/beyondnow-sn/beyondnow-private-coaching"
             features={[
                 'Overall discuss your job search strategy',
                 'Technical support for ServiceNow-related PDI project questions',
