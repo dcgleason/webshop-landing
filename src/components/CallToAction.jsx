a6ec04006ab71a397e4eb16ef1bb3791ca58f608
dcgleason <danny.c.gleason@gmail.com>
2023-10-26 14:15:59 -0400

    uipdate

--- src/components/CallToAction.jsx
+++ src/components/CallToAction.jsx
@@ -21,10 +21,10 @@ export function CallToAction() {
       <Container className="relative">
         <div className="mx-auto max-w-lg text-center">
           <h2 className="font-display text-3xl tracking-tight text-white sm:text-4xl">
-          Insights based on what works and real world insights.
-          </h2>
+          "Dan always goes the extra mile" 
+                    </h2>
           <p className="mt-4 text-lg tracking-tight text-white">
-            Book a call today.
+            - past mentee
           </p>
           <Button href="/#pricing" color="white" className="mt-10">
             Get started
