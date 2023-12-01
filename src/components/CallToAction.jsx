fad4c7de23f44cd9eb044c7eadf838b169256474
dcgleason <danny.c.gleason@gmail.com>
2023-12-01 17:15:52 -0500

    update error

--- src/components/CallToAction.jsx
+++ src/components/CallToAction.jsx
@@ -21,7 +21,7 @@ export function CallToAction() {
       <Container className="relative">
         <div className="mx-auto max-w-lg text-center">
           <h2 className="font-display text-3xl tracking-tight text-white sm:text-4xl">
-          &quot;[Dan] gave me real truths to the career that I&apos;m grateful for that. [Dan&apos;s] an awesome guy...and really gave me the confidence I needed (throughout the process)&quot;
+          &quot;[Dan] gave me real truths to the career that I&apos;m grateful for. [Dan&apos;s] an awesome guy...and really gave me the confidence I needed (throughout the process)&quot;
                     </h2>
           <p className="mt-4 text-lg tracking-tight text-white">
             - Adam H., Junior ServiceNow Developer (past client, got a job in 3 weeks)
