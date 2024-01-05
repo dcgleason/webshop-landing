294af8ad15c30acbd61a19ada25ab67197dcb256
Danny Gleason <dannygleason@Dannys-Laptop.local>
2024-01-05 10:12:09 -0500

    update

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -160,7 +160,7 @@ export function Pricing() {
               'LinkedIn direct message message reviews and feedback to help you more effectively build relationships with key decision makers',
               '48 hour max response time + 30 min of 1:1 coaching per week',
                         ]}
-           buttonText="Book call & Revise my resume for free"
+           buttonText="Revise my resume for free & book call"
 
           />
         </div>
