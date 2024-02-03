61746f5f1126243ec32f3609657cdd474929f0bd
Danny Gleason <dannygleason@Dannys-Laptop.local>
2024-02-03 03:02:33 -0500

    update docuemtnjs

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,11 +1,6 @@
-import { Head, Html, Main, NextScript } from 'next/document'
-import Script from 'next/script'
+import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document(props) {
-  let pageProps = props.__NEXT_DATA__?.props?.pageProps
-
-  
-
   return (
     <Html
       className="h-full scroll-smooth bg-white antialiased [font-feature-settings:'ss01']"
@@ -22,13 +17,20 @@ export default function Document(props) {
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Lexend:wght@400;500&display=swap"
         />
+        {/* Insert the new script here */}
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            (function(w){w.fpr=w.fpr||function(){w.fpr.q=w.fpr.q||[];w.fpr.q[arguments[0]=='set'?'unshift':'push'](arguments);};})(window);
+            fpr("init", {cid:"1wyzxuko"}); 
+            fpr("click");
+          `
+        }} />
+        <script src="https://cdn.firstpromoter.com/fpr.js" async></script>
       </Head>
       <body className="flex h-full flex-col">
         <Main />
         <NextScript />
-      
-       
       </body>
     </Html>
-  )
+  );
 }
