2d9203b6ea9cd0409e086342059a1c66cd615500
Danny Gleason <dannygleason@Dannys-Laptop.local>
2024-02-09 20:46:38 -0500

    update

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -179,7 +179,7 @@ export function Pricing({ clientSecret, stripePromise}) {
           features={[
               'Up to 25 jobs applied on your behalf per month',
               'Live weekly group coaching calls by a Sr. ServiceNow Engineer to help you with your job search',
-              '1:1 support from a Sr. ServiceNow Engineer for personalized resume reviews and advice ($49/session)',
+              '1:1 support from a Sr. ServiceNow Engineer for personalized advice, resume reviews, or technical support ($49/session)',
               'A new PDI project idea + user stories every 2 weeks to help you build your portfolio',
               'Resume revision & cover letter checklists + resume template to help you stand out to recruiters',
               'ServiceNow mini-course (detailed notes summary + flashcards of NowLearning classes) released every month to help you study for your next ServiceNow certification',
