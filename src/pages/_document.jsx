a128c36fc586c6a8c6e1de4e96eaa40beda47990
Danny Gleason <dannygleason@Dannys-Laptop.local>
2024-02-11 08:51:41 -0500

    update

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -17,7 +17,7 @@ export default function Document(props) {
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Lexend:wght@400;500&display=swap"
         />
-        {/* Insert the new script here */}
+        {/* First Promoter Script */}
         <script dangerouslySetInnerHTML={{
           __html: `
           (function(w){w.fpr=w.fpr||function(){w.fpr.q = w.fpr.q||[];w.fpr.q[arguments[0]=='set'?'unshift':'push'](arguments);};})(window);
@@ -26,6 +26,10 @@ export default function Document(props) {
           `
         }} />
         <script src="https://cdn.firstpromoter.com/fpr.js" async></script>
+        {/* Reddit Pixel */}
+        <script dangerouslySetInnerHTML={{ __html: `
+          !function(w,d){if(!w.rdt){var p=w.rdt=function(){p.sendEvent?p.sendEvent.apply(p,arguments):p.callQueue.push(arguments)};p.callQueue=[];var t=d.createElement("script");t.src="https://www.redditstatic.com/ads/pixel.js",t.async=!0;var s=d.getElementsByTagName("script")[0];s.parentNode.insertBefore(t,s)}}(window,document);rdt('init','a2_eh3xidnhsto8');rdt('track', 'PageVisit');
+        ` }} />
       </Head>
       <body className="flex h-full flex-col">
         <Main />
