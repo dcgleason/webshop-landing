e006a4ba339f34a2cf0deaf78cb8dc9c29d0beae
Danny Gleason <dannygleason@Dannys-Laptop.local>
2024-02-14 16:29:46 -0500

    update

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -178,7 +178,7 @@ export function Pricing({ clientSecret, stripePromise}) {
           description="Community-based job search support from experienced ServiceNow professionals. Cancel anytime"
           features={[
               'Get personlized job search support from experienced ServiceNow professionals (resume reviews, interview prep, career advice, etc.)',
-              'Build relationships via small group masterminds that build your network and resolve personal blockers for you',
+              'Build relationships via small group masterminds that resolve personal blockers for you',
               'Get a new PDI project idea every 2 weeks to build (with the assistance of a Sr. ServiceNow Developer)',
               "Access NowLearning course summaries and flashcards to help you study and pass ServiceNow certifications",
               "Get exlucsive access to software bot that automates the LinkedIn job search process (coming soon)",
