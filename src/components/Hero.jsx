e6edc9a62d0a1f281462647bcbdb0997d5d15541
Danny Gleason <dannygleason@Dannys-Laptop.local>
2024-02-15 16:04:01 -0500

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -31,7 +31,7 @@ export function Hero() {
         your ServiceNow development career.
       </h1>
       <p className="mx-auto mt-6 max-w-2xl text-lg tracking-tight text-slate-700">
-     BeyondNow is a U.S.-based, career coaching firm that utilizes advice & knowledge from expert ServiceNow engineers and community-based support to level up in your career faster. 
+     BeyondNow is a U.S.-based, career coaching firm for ServiceNow professionals that utilizes advice & knowledge from expert ServiceNow engineers and community-based support to level up in your career faster. 
       </p>
         <div className="mt-10 flex justify-center gap-x-6">
         <Button href="#pricing">Join the ServiceNow Career Builders community today</Button>
